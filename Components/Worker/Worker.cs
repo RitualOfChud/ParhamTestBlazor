89999257f884c0f338331f6059f7e10f75445091
ritua <ritua@OMEN>
2024-02-15 15:57:48 +0100

    Add project files.

---
+++ Components/Worker/Worker.cs
@@ -0,0 +1,9 @@
+﻿namespace ParhamTestBlazor.Components.Worker
+{
+    public class Worker
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname { get { return $"{Firstname} {Lastname}"; } }
+    }
+}
